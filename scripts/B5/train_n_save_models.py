d946091a003914c068a8f28eaff312bc365e636e
NotInvalidUsername <ryannjh99@gmail.com>
2025-04-02 21:00:31 +0800

    Renamed ml_1.py to train_n_save_models.py

--- scripts/B5/train_n_save_models.py
+++ scripts/B5/train_n_save_models.py
@@ -1,11 +1,9 @@
 from custom_functions import evaluate_model
-#from models import models_1 as models
 from ml_1 import *
 from Imports import pd, os, plt
 
 from Imports import (MultiOutputRegressor, XGBRegressor, GradientBoostingRegressor, 
                      RandomForestRegressor, SVR, MLPRegressor, dump)
-from custom_functions import create_neural_network, adjusted_r2_score
 
 models = {
     "XGBoost": MultiOutputRegressor(XGBRegressor(
@@ -54,7 +52,7 @@ def display_results1():
 
         # Save model to script directory
         model_filename = os.path.join(script_dir, f'{name.lower().replace(" ", "_")}_model.joblib')
-        # dump(model, model_filename)
+        dump(model, model_filename)
         print(f"Saved {name} model to {model_filename}")
         mse, r2, adj_r2, figures = evaluate_model(model, X_test_new, y_test_reg_new, coord_scaler, name) # X_val, y_val_reg can we swapped with X_test_new, y_test_reg_new
         for fig in figures:
