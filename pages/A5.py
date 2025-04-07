a5e90c3d923020d887558d0cb963450601c3df51
clarakan <e0970181@u.nus.edu>
2025-04-07 23:19:40 +0800

    Added dropdowns for frontend

--- pages/A5.py
+++ pages/A5.py
@@ -48,81 +48,89 @@ with tab1:
     st.header("Periodic Trends")
     st.markdown("In this section, we highlight the more significant periodic trends we have observed using attendance data from 2 theme parks, PortAventura World and Tivoli Gardens. We found that **:blue[weekends and holidays have significantly higher attendance]** than other days as most people do not have to go to work or school and hence have the time to go to theme parks.")
 
-    st.markdown("#### Overview")
-    st.markdown("The Covid-19 pandemic which started in March 2020 and ended in May 2023 affected data within those years as shown in the dip in 2020 and saw 0 attendance for some periods in 2020 and 2021. It also took some time for attendance to increase to pre-pandemic levels.")
-    st.pyplot(tivoli_PortAventura_overview(attendance_data))
+    with st.expander("##### Overview: Impact of COVID-19"):
+        st.markdown(
+            "The Covid-19 pandemic which started in March 2020 and ended in May 2023 affected data within those years as shown in the dip in 2020 and saw 0 attendance for some periods in 2020 and 2021. "
+            "It also took some time for attendance to increase to pre-pandemic levels."
+        )
+        st.pyplot(tivoli_PortAventura_overview(attendance_data))
 
     #st.markdown("#### Monthly Trend for each year")
     #st.markdown("Attendance from both parks follows the same trend roughly. Before Covid-19, summer break and winter break periods saw an increase in attendance.")
 
     #st.pyplot(tivoli_PortAventura_all_time(attendance_data))
 
-    st.markdown("#### Daily Trend")
-    st.markdown("The **:blue[weekends have higher attendance]** with **:blue[Saturday being the most popular day]** to visit the park.")
-    st.pyplot(tivoli_PortAventura_weekly_trend_mean(attendance_data))
+    with st.expander("##### Daily Trend"):
+        st.markdown(
+            "The **:blue[weekends have higher attendance]**, with **:blue[Saturday being the most popular day]** to visit the park."
+        )
+        st.pyplot(tivoli_PortAventura_weekly_trend_mean(attendance_data))
 
     st.markdown("#### Local events and holidays")
     st.markdown("**:blue[Attendance on holidays are higher]** than on non-holidays. Among holidays, holidays such as Christmas and New Years are more popular times where people go to the parks.")
-    st.markdown("##### PortAventura World (Spain)")
-    st.markdown("PortAventura World which is in Spain sees **:blue[higher attendance during Christmas]**, Labor Day and New Year's. The boxplot shows that the difference between holidays vs non-holidays is stastically significant. ")
 
-    col1, col2 = st.columns(2)  # Create two equal-width columns
+    with st.expander("##### PortAventura World (Spain)"):
+        st.markdown("PortAventura World which is in Spain sees **:blue[higher attendance during Christmas]**, Labor Day and New Year's. The boxplot shows that the difference between holidays vs non-holidays is stastically significant. ")
 
-    with col1:
-        st.markdown("##### Statistical Test for Holidays - PortAventura")
-        st.pyplot(port_aventura_holidays_stat_test(attendance_data))
+        col1, col2 = st.columns(2)  # Create two equal-width columns
 
-    with col2:
-        st.markdown("##### Holiday Attendance Trends - PortAventura")
-        st.pyplot(port_aventura_holidays_plot(attendance_data))
+        with col1:
+            st.markdown("###### Statistical Test for Holidays - PortAventura")
+            st.pyplot(port_aventura_holidays_stat_test(attendance_data))
 
-    st.markdown("##### Tivoli Gardens (Denmark)")
-    st.markdown("Tivoli Gardens which is in Denmark sees **:blue[higher attendance during Christmas, Boxing Day and New Year's]**. The boxplot shows that the difference between holidays vs non-holidays is stastically significant. ")
+        with col2:
+            st.markdown("###### Holiday Attendance Trends - PortAventura")
+            st.pyplot(port_aventura_holidays_plot(attendance_data))
+
+    with st.expander("##### Tivoli Gardens (Denmark)"):
+        st.markdown("Tivoli Gardens which is in Denmark sees **:blue[higher attendance during Christmas, Boxing Day and New Year's]**. The boxplot shows that the difference between holidays vs non-holidays is stastically significant. ")
     
-    col1, col2 = st.columns(2)  # Create two equal-width columns
+        col1, col2 = st.columns(2)  # Create two equal-width columns
 
-    with col1:
-        st.markdown("##### Statistical Test for Holidays - Tivoli Gardens")
-        st.pyplot(tivoli_holidays_stat_test(attendance_data))
-    
-    with col2:
-        st.markdown("##### Holiday Attendance Trends - Tivoli Gardens")
-        st.pyplot(tivoli_holidays_plot(attendance_data))
+        with col1:
+            st.markdown("###### Statistical Test for Holidays - Tivoli Gardens")
+            st.pyplot(tivoli_holidays_stat_test(attendance_data))
+        
+        with col2:
+            st.markdown("###### Holiday Attendance Trends - Tivoli Gardens")
+            st.pyplot(tivoli_holidays_plot(attendance_data))
+
 
 with tab2:
     st.header("Sentiment and Ratings")
-    st.markdown("In this section, we highlight the different ratings and sentiments of visitors throughout different months using Disneyland Review Data which consists of 3 branches which are Disneyland Paris, Hong Kong and California. Sentiments and Ratings generally do not change much month to month. It is also found that Disneyland California has the highest ratings followed by Hong Kong and then Paris.")
-    st.markdown("##### Bar Plots of Sentiments and Ratings")
-    st.markdown("The **:blue[sentiments and ratings do not vary drastically between different months]**. August has the lowest sentiment and rating among all the months. November has the highest rating and September has the best sentiment score.")
-    col1, col2 = st.columns(2)  # Create two equal-width columns
-
-    with col1:
-        st.markdown("##### Monthly Sentiment Scores")
-        st.pyplot(monthly_sentiment_scores(DisneylandReviews_data))
+    st.markdown("In this section, we highlight the different ratings and sentiments of visitors throughout different months using Disneyland Review Data which consists of 3 branches which are Disneyland Paris, Hong Kong and California. **:blue[Sentiments and Ratings generally do not change much month to month.]** It is also found that Disneyland California has the highest ratings followed by Hong Kong and then Paris.")
     
-    with col2:
-        st.markdown("##### Monthly Rating Scores")
-        st.pyplot(monthly_rating_scores(DisneylandReviews_data))
+    with st.expander("##### Bar Plots of Sentiments and Ratings"):
+        st.markdown("The **:blue[sentiments and ratings do not vary drastically between different months]**. August has the lowest sentiment and rating among all the months. November has the highest rating and September has the best sentiment score.")
+        col1, col2 = st.columns(2)  # Create two equal-width columns
 
-    st.markdown("#### Heatmaps for Ratings")
-    st.markdown("To further investigate, a heatmap is used to check rating differences for each branch. California and Hong Kong in general have higher ratings on average.")
-    
-    st.markdown("##### Heatmap for each month and year for every branch")
-    figs = list(heatmap_ratings(DisneylandReviews_data))  # Convert generator to list
-    cols = st.columns(len(figs))  # Create columns dynamically
+        with col1:
+            st.markdown("###### Monthly Sentiment Scores")
+            st.pyplot(monthly_sentiment_scores(DisneylandReviews_data))
+        
+        with col2:
+            st.markdown("###### Monthly Rating Scores")
+            st.pyplot(monthly_rating_scores(DisneylandReviews_data))
 
-    for col, fig in zip(cols, figs):  
-        with col:
-            st.pyplot(fig)
-    
-    st.markdown("##### Average rating per month for every branch")
-    figs = list(heatmap_month_branch_ratings(DisneylandReviews_data))  # Convert generator to list
-    cols = st.columns(len(figs))  # Create columns dynamically
+    with st.expander("##### Heatmaps for Ratings"):
+        st.markdown("To further investigate, a heatmap is used to check rating differences for each branch. California and Hong Kong in general have higher ratings on average.")
+        
+        st.markdown("###### Heatmap for each month and year for every branch")
+        figs = list(heatmap_ratings(DisneylandReviews_data))  # Convert generator to list
+        cols = st.columns(len(figs))  # Create columns dynamically
 
-    for col, fig in zip(cols, figs):  
-        with col:
-            st.pyplot(fig)
+        for col, fig in zip(cols, figs):  
+            with col:
+                st.pyplot(fig)
     
+    with st.expander("##### Average rating per month for every branch"):
+        figs = list(heatmap_month_branch_ratings(DisneylandReviews_data))  # Convert generator to list
+        cols = st.columns(len(figs))  # Create columns dynamically
+
+        for col, fig in zip(cols, figs):  
+            with col:
+                st.pyplot(fig)
+        
 
 
 
@@ -132,79 +140,78 @@ with tab3:
     
     #Continent
     st.markdown("##### Continent of Origin Demographics")
-    st.markdown("###### Continent and Month of Visit")
-    st.markdown("People from North America and Europe tend to leave the most reviews consistently.")
+    with st.expander("##### Continent and Month of Visit"):
+        st.markdown("People from North America and Europe tend to leave the most reviews consistently.")
+        st.pyplot(continent_month_plot(DisneylandReviews_data))
 
-    st.pyplot(continent_month_plot(DisneylandReviews_data))
+    with st.expander("##### Continent of Origin and Rating/Sentiment Score"):
+        st.markdown("People from North America and Oceania tend to give higher ratings while Europeans gave the lowest ratings.")
 
-    st.markdown("###### Continent of Origin and Rating/Sentiment Score")
-    st.markdown("People from North America and Oceania tend to give higher ratings while Europeans gave the lowest ratings.")
+        col1, col2 = st.columns(2)  # Create two equal-width columns
 
-    col1, col2 = st.columns(2)  # Create two equal-width columns
-
-    with col1:
-        st.pyplot(continent_rating(DisneylandReviews_data))
-    
-    with col2:
-        st.pyplot(continent_sentiment(DisneylandReviews_data))
+        with col1:
+            st.pyplot(continent_rating(DisneylandReviews_data))
+        
+        with col2:
+            st.pyplot(continent_sentiment(DisneylandReviews_data))
 
     #Visit type
-    st.markdown("#### Visit Type Demographics")
-    st.markdown("Visitors were split into 5 types: Families, Couples, Friends, Solo and Unknown based on their reviews. Most people travel with their families")
+    st.markdown("##### Visit Type Demographics")
+    st.markdown("Visitors were split into 5 types: Families, Couples, Friends, Solo and Unknown based on their reviews. Most reviewers tend to travel with their families.")
     
-    st.markdown("##### Visit Type and Month of Visit")
-    st.markdown("Based on the reviews, most people travel with their family with June to August, October and December being the popular months. This is in line with school holidays such a Summer and Winter break. Guest going with their families tend to give the most reviews.")
+    with st.expander("##### Visit Type and Month of Visit"):
+        st.markdown("Based on the reviews, most people travel with their family with June to August, October and December being the popular months. This is in line with school holidays such a Summer and Winter break. Guest going with their families tend to give the most reviews.")
 
-    st.pyplot(visit_type_month(DisneylandReviews_data))
+        st.pyplot(visit_type_month(DisneylandReviews_data))
 
-    st.markdown("##### Visit Type and Ratings/Sentiment Scores")
-    st.markdown("Families tend to give poor ratings compared to other visit types although their sentiment score is not the lowest. It might be useful for theme park operators to investigate this discrepancy.")
+    with st.expander("##### Visit Type and Ratings/Sentiment Scores"):
+        st.markdown("Families tend to give poor ratings compared to other visit types although their sentiment score is not the lowest. It might be useful for theme park operators to investigate this discrepancy.")
 
-    col1, col2 = st.columns(2)  # Create two equal-width columns
+        col1, col2 = st.columns(2)  # Create two equal-width columns
 
-    with col1:
-        st.pyplot(visit_type_rating(DisneylandReviews_data))
+        with col1:
+            st.pyplot(visit_type_rating(DisneylandReviews_data))
 
-    with col2:
-        st.pyplot(visit_type_sentiment(DisneylandReviews_data))
+        with col2:
+            st.pyplot(visit_type_sentiment(DisneylandReviews_data))
 
 
 with tab4:
     st.header("Ratings vs Attendance")
-    st.markdown("#### Does higher ratings mean more attendance?")
-    st.markdown("We investigate this question using yearly Disneyland attendance data from the 3 branches and Disneyland Review data.  The correlation value of 0.525 suggests that there is a **:blue[moderate positive relationship between previous year ratings and next year's attendance]** .")
-    merged_df = merged_disney(attendance_disney, DisneylandReviews_data)
-    st.pyplot(correlation_plot(merged_df))
+    with st.expander("##### Does higher ratings mean more attendance?"):
+        st.markdown("We investigate this question using yearly Disneyland attendance data from the 3 branches and Disneyland Review data.  The correlation value of 0.525 suggests that there is a **:blue[moderate positive relationship between previous year ratings and next year's attendance]** .")
+        merged_df = merged_disney(attendance_disney, DisneylandReviews_data)
+        st.pyplot(correlation_plot(merged_df))
 
 with tab5:
     st.header("Conclusion")
-    st.markdown("#### Key Insights")
-    st.markdown(
-    """   
-    - Attendance peaks on Holidays, Saturday is also the most popular day of the week to visit.
-    - Families and general visitors attend more during summer and winter breaks.
-    - Families tend to give the most reviews however their ratings are lower compared to other visit types.
-    - North American guests and European guests give the most ratings among the different continents of origin. North Americans giving higher ratings while European guests give lower ratings.
-    - Ratings are positively correlated with future attendance.
-    """
-    )
 
+    with st.expander("##### 🔍 Key Insights"):
+        st.markdown(
+            """   
+            - Attendance peaks on Holidays, Saturday is also the most popular day of the week to visit.  
+            - Families and general visitors attend more during summer and winter breaks.  
+            - Families tend to give the most reviews however their ratings are lower compared to other visit types.  
+            - North American guests and European guests give the most ratings among the different continents of origin. North Americans giving higher ratings while European guests give lower ratings.  
+            - Ratings are positively correlated with future attendance.
+            """
+        )
 
-    st.markdown("#### Business Recommendations")
-    st.markdown(
-    """   
-    - Implement dynamic pricing, fast passes, and increase staffing on peak days for better crowd management.
-    - Introduce seasonal events and off-season discounts to balance demand and optimize attendance.
-    - Parks can tailor promotions and packages to different groups of visitors.
-    - Investigate the pain points from reviews to better improve the parks. This allows the theme park to obtain better ratings and higher attendance in the future.
-    - Parks who have lower ratings such as Disneyland Paris can research what parks with better ratings such as Disneyland California and Hong Kong are doing right to improve their guest satisfaction.
-    """
-    )
+    with st.expander("##### 💼 Business Recommendations"):
+        st.markdown(
+            """   
+            - Implement dynamic pricing, fast passes, and increase staffing on peak days for better crowd management.  
+            - Introduce seasonal events and off-season discounts to balance demand and optimize attendance.  
+            - Parks can tailor promotions and packages to different groups of visitors.  
+            - Investigate the pain points from reviews to better improve the parks. This allows the theme park to obtain better ratings and higher attendance in the future.  
+            - Parks who have lower ratings such as Disneyland Paris can research what parks with better ratings such as Disneyland California and Hong Kong are doing right to improve their guest satisfaction.
+            """
+        )
 
-    st.markdown("#### Limitations and Improvements")
-    st.markdown(
-    """
-    - Analysis would have been more insightful if the data obtained could be all from the same parks. All Disneyland Data would have been more specific however Disneyland Monthly Data could not be obtained
-    - There was some difficulty in accurately splitting visitors by visit type based on what they said in reviews. Parks can consider giving a visit type option in their reviews to understand their bigger demographics and cater to them more.
-    """
-    )
+    with st.expander("##### ⚠️ Limitations and Improvements"):
+        st.markdown(
+            """
+            - Analysis would have been more insightful if the data obtained could be all from the same parks. All Disneyland Data would have been more specific however Disneyland Monthly Data could not be obtained.  
+            - There was some difficulty in accurately splitting visitors by visit type based on what they said in reviews. Parks can consider giving a visit type option in their reviews to understand their bigger demographics and cater to them more.
+            """
+        )
